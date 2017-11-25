fe4a9e5677234be55fe9a6bbb2ed2989eca8e02a
anthony <arnygora@gmail.com>
2017-11-25 02:01:28 +0200

    fix header hw_7

--- HW_7/js/bootstrap.js
+++ HW_7/js/bootstrap.js
@@ -3810,7 +3810,7 @@ var Tab = function () {
 
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v4.0.0-alpha.6): index.js
+ * Bootstrap (v4.0.0-alpha.6): 01_index.js
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
