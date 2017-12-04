90f98a1fd3c22f37145fd6d52c79b6be97523e5f
anthony <arnygora@gmail.com>
2017-12-05 00:09:11 +0200

    yuk

---
+++ HW_8/js/script.js
@@ -0,0 +1,88 @@
+function buttonClickHandler(event)
+{
+	let elementId = event.target.id;
+    handlerEvent(elementId)
+}
+
+$('button').click(buttonClickHandler);
+
+function handlerEvent(elementId) {
+	switch(elementId) {
+  case 'add': {
+  	$('.block').addClass('text-format');
+    break;
+  }
+  case 'remove': {
+      $('.block').removeClass('text-format');
+      break;
+  }
+  case 'toggle': {
+      $('.block').toggleClass('text-format');
+      break;
+  }
+  case 'get': {
+	  let attr = $('#get').attr('id');
+	  $('p').text('ID of button "getAttr" = ' + attr);
+      break;
+  }
+  case 'set': {
+	  $('#set').attr('class', 'button-style');
+      break;
+  }
+  case 'alert': {
+      alert('Message called by click on "alertOnClick" button');
+      break;
+  }
+  case 'trigger': {
+      $('#alert').trigger('click');
+      break;
+  }
+  case 'clone': {
+      $('#clone').clone().appendTo('li.clone').text('cloned').css('display', 'block');
+        break;
+    }
+    case 'closest': {
+      let parent = $('#closest').closest('ul');
+      $('p').text(parent[0]);
+      console.log(parent);
+        break;
+    }
+    case 'each': {
+        let buttonArray = $('.buttons li button');
+        let text = "";
+        $(buttonArray).each(function(index, element){
+            text += " " + $(element).text();
+        });
+        $('p').text(text);
+        console.log(text);
+        break;
+    }
+    case 'find': {
+        let findButton = $('.buttons').find('find');
+        console.log(findButton);
+        break;
+    }
+    case 'fadeIn': {
+        $( ".fadein-text" ).fadeIn(2000);
+        break;
+    }
+    case 'fadeOut': {
+        $( ".fadeout-text" ).fadeOut(2000);
+        break;
+    }
+    case 'hide': {
+        $( ".hide-text" ).hide(2000);
+        break;
+    }
+    case 'show': {
+        $( ".show-text" ).show(2000);
+        break;
+    }
+    case 'data': {
+        console.log(arguments);
+        break;
+    }
+  default:
+    break
+	}
+}
