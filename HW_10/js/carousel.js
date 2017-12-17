d7bac916ce87bd6cb3cec26c6c6346b76d36f42b
anthony <arnygora@gmail.com>
2017-12-17 23:49:55 +0200

    added slider

---
+++ HW_10/js/carousel.js
@@ -0,0 +1,21 @@
+$(".slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+    $('.slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('.slideshow');
+}, 10000);
+
+$(".cite-block > div:gt(0)").hide();
+
+setInterval(function() {
+    $('.cite-block > div:first')
+        .fadeOut(10)
+        .next()
+        .fadeIn(10)
+        .end()
+        .appendTo('.cite-block');
+}, 10000);
