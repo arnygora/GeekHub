04d7133b809d9f363fbe7ed2e881b477d1a65806
anthony <arnygora@gmail.com>
2018-01-15 21:09:57 +0200

    added hw_13

---
+++ HW_13/www/app/js/icon.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#nav-icon').click(function(){
+        $(this).toggleClass('open');
+        $('.hide-block').toggleClass('hidden-block');
+        $('.hide-list').slideToggle(0);
+    });
+});
+
