7d5d576ac7eadede297c875de34cbd1082ea6e18
anthony <arnygora@gmail.com>
2018-02-02 11:03:27 +0200

    Added hw 14 (react)

--- HW_14 (react)/www/src/App.js
+++ HW_14 (react)/www/src/App.js
@@ -24,3 +24,22 @@ class App extends React.Component {
 }
 
 export default App;
+
+// import Header from './Header';
+//
+// const hero = [
+//     <div className="hero upper-text">
+//         <p>we are leading consulting company</p>
+//         <h2>choose your industry</h2>
+//         <ul className="servises">
+//             <li><span className="fa fa-book fa-4x" ></span><p className="upper-text">education</p></li>
+//             <li><span className="fa fa-briefcase fa-4x"></span><p className="upper-text">business</p></li>
+//             <li><span className="fa fa-laptop fa-4x"></span><p className="upper-text">technology</p></li>
+//             <li><span className="fa fa-money fa-4x"></span><p className="upper-text">banking</p></li>
+//         </ul>
+//     </div> ];
+//
+// <Header>
+//     {hero}
+// </Header>
+
