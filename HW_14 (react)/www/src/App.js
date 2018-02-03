efa067a8d062d5e2125b9cd1655efe147c38a0d2
anthony <arnygora@gmail.com>
2018-02-03 04:09:07 +0200

    Added hw 14 (react)

--- HW_14 (react)/www/src/App.js
+++ HW_14 (react)/www/src/App.js
@@ -7,17 +7,20 @@ import Testimonials from './Testimonials';
 import Footer from './Footer';
 
 class App extends React.Component {
-
     render() {
-        const article = '<h3>title</h3>';
-
+        const article = <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad adipisci est expedita</h3>
         return (
             <div className="App">
+                {article}
                 <Header>
                     {article}
                 </Header>
-                <Testimonials />
-                <Footer />
+                <Testimonials>
+                    {article}
+                </Testimonials>
+                <Footer>
+                    {article}
+                </Footer>
             </div>
         );
     }
@@ -25,21 +28,4 @@ class App extends React.Component {
 
 export default App;
 
-// import Header from './Header';
-//
-// const hero = [
-//     <div className="hero upper-text">
-//         <p>we are leading consulting company</p>
-//         <h2>choose your industry</h2>
-//         <ul className="servises">
-//             <li><span className="fa fa-book fa-4x" ></span><p className="upper-text">education</p></li>
-//             <li><span className="fa fa-briefcase fa-4x"></span><p className="upper-text">business</p></li>
-//             <li><span className="fa fa-laptop fa-4x"></span><p className="upper-text">technology</p></li>
-//             <li><span className="fa fa-money fa-4x"></span><p className="upper-text">banking</p></li>
-//         </ul>
-//     </div> ];
-//
-// <Header>
-//     {hero}
-// </Header>
 
