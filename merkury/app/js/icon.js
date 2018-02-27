ee15da525570b27875e4be66b37a1b2d609dfd45
anthony <arnygora@gmail.com>
2018-02-27 10:26:58 +0200

    ;

--- merkury/app/js/icon.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('#nav-icon').click(function(){
-        $(".navy").toggleClass("hidden-block");
-        $(".mainContent").toggleClass("col-12");
-        $(this).toggleClass('open');
-    });
-});
